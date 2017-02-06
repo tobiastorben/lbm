b5c4fdd7c15441624ee2e0d0ad87669816e457a6
tobias <t_torben@hotmail.com>
2017-02-06 19:54:49 +0100

    Partially working version. Some problems with the streaming on bondaries.

--- src/lbm.c
+++ src/lbm.c
@@ -8,70 +8,69 @@
 
 int main(int argc, char** argv) {
 	//Define grid
-	int nx = 10;
-	int ny = 5;
-	//Define obstacle //Remove??
-	int cylX  = nx/5+1;
-	int cylY = ny/2 + 3;
-	int cylR = ny/10+1;
-	
+	int nx = 400;
+	int ny = 100;
+	double tull;
 	//Define flow parameters
 	double uIn = 0.1;
 	double Re = 100;
-	double nu = uIn*cylR/Re;
+	double nu = uIn*2*(ny/10 +1)/Re;
 	double tau = 1/(3*nu+0.5);//Relaxation parameter of BGK collision operator
 	
 	//Define simulations parameters
-	int nIter = 4;
+	int nIter = 10000;
 	int cyclesPerWrite = 5;
 	
 	
 	//D2Q9 lattice constants
 	int nf = 9;
 	double w[] = {4.0/9, 1.0/9, 1.0/9, 1.0/9, 1.0/9, 1.0/36, 1.0/36, 1.0/36, 1.0/36};//Weights
-	int ex[] = {0,1,0,-1,0,1,-1,-1,1};//x component of lattice vectors 
-	int ey[] = {0,0,1,0,-1,1,1,1,-1};//y component of lattice vectors
+	double ex[] = {0,1.0,0,-1.0,0,1.0,-1.0,-1.0,1.0};//x component of lattice vectors 
+	double ey[] = {0,0,1.0,0,-1.0,1.0,1.0,-1.0,-1.0};//y component of lattice vectors
+	int cx[] = {0,1,0,-1,0,1,-1,-1,1};//x component of lattice vectors 
+	int cy[] = {0,0,1,0,-1,1,1,-1,-1};//y component of lattice vectors
 	int opposite[] = {0,3,4,1,2,7,8,5,6};//Index of opposite vector in lattice
 	
 	//BC data
-	int nBBcells = 2;
-	//int** bbCells = readObstacleData(nBBcells);//Array of indices to bounce back nodes
-	int** bbCells = (int **)malloc(2 * sizeof(int*));
-	for(int  i = 0; i < 2; i++){
-		bbCells[i] = (int *)malloc(2 * sizeof(int));
-	};
-	bbCells[0][0] = 1;
-	bbCells[0][1] = 2;
-	bbCells[1][0] = 3;
-	bbCells[1][1] = 4;
-	int indInlet = 1;
-	int indOutlet = nx;
-	
+	int nBBcells = 377;
+	int** bbCells = readObstacleData(nBBcells);//Array of indices to bounce back nodes
 	//ICs: Initialze flow as Poiseuille flow
-	double H = ny-2;
 	double initialRho = 1;
 	double** rho = initRho(nx,ny,initialRho);
 	double** ux = initUx(nx,ny,uIn);
 	double** uy = initUy(nx,ny);
 	double*** fIn = initFin(nf,nx,ny,ex,ey,ux,uy,w,rho);
 	double*** fOut = initFout(nx,ny,nf);
-
-	//Main loop
-	for (int i = 0; i < nIter; i++){		
-		//Update macroscopic variables
-		updateRho(rho,fIn,nf,nx,ny);
+	
+	//Main loop. Marches flow in time
+	for (int i = 0; i < nIter; i++){	
+		//Update macroscopic variables	
+		updateRho(rho,fIn,nx,ny,nf);		
 		updateU(ux,uy,fIn,rho,ex,ey,nx,ny,nf);
-		
+	
 		//Apply BCs
-		inlet(ux,uy,rho,fIn,uIn,nx,ny);//Poiseuille (Zou/He)
+		inlet(ux,uy,rho,fIn,uIn,nx,ny);//Poiseuille (Zou/He)		
 		outlet(ux,uy,rho,fIn,nx,ny);//Constant pressure (Zou/He)
 		
 		//Collide (Bhatnagar-Gross-Kroot model)
-		collide(ux,uy,fIn,fOut,rho,ex,ey,nx,ny,nf,tau,w);//Particle-Particle collisions
+		collide(ux,uy,fIn,fOut,rho,ex,ey,nx,ny,nf,tau,w);//Particle-Particle collisions				
 		bounce(fIn,fOut,nx,ny,nf,bbCells,nBBcells,opposite);//Bounce-back collision with boundary
+		csvWriteD(fIn[1],nx,ny, "fin.csv");
+		printf("Prestream printed\n");
+		printf("Press any key to continue:\n");
+		getchar();
+		//Streaming
+		stream(fIn,fOut,cx,cy,nx,ny,nf);		
+		csvWriteD(fIn[1],nx,ny, "fin.csv");
+		printf("Poststream printed\n");
+		printf("Press any key to continue:\n");
+		getchar();
 	}
-	
-	printMatD(rho,nx,ny);	
+	csvWriteD(ux,nx,ny, "ux.csv");
+	csvWriteD(uy,nx,ny, "uy.csv");
+	csvWriteD(rho,nx,ny, "rho.csv");
+	csvWriteD(fIn[0],nx,ny, "fin.csv");
+	csvWriteD(fOut[0],nx,ny, "fout.csv");
 	return 0;
 }
 
--- src/utilities.c
+++ src/utilities.c
@@ -5,20 +5,34 @@
 
 //Reads geometry of bounce back cells from a file, stores it in a vector
 //of indices
-int* readObstacleData(int nBBcells) {
-	FILE* fp = fopen("obst.dat","r");
-	char* line = malloc(nBBcells*100);
-	fgets(line,nBBcells*100,fp);
-	char* token = strtok(line, ",");
-	int* ind = malloc(nBBcells*sizeof(int));
+int** readObstacleData(int nBBcells) {
+	FILE* fp = fopen("obstacle.csv","r");
+	char* line = malloc(nBBcells*100);		
+	char* token;
+	int** bbCells = (int **)malloc(2 * sizeof(int*));
+	for(int l = 0; l < 2; l++){
+		bbCells[l] = (int *)malloc(nBBcells * sizeof(int));
+	}
+
 	int i = 0;
-	 while( token != NULL ) 
-   {
-    ind[i] = atoi(token);
-	token = strtok(NULL, ",");
+	int j = 0;
+	int k = 0;
+	while (!feof(fp)){
+		fgets(line,nBBcells*100,fp);
+		token = strtok(line, ",");		
+		int j = 0;
+		while( token != NULL ){ 	    
+		if (atoi(token)) {
+			bbCells[0][k] = i;
+			bbCells[1][k] = j;
+			k++;
+		}
+		token = strtok(NULL, ",");
+		j++;
+	    }
 	i++;
-   }
-	return ind;
+	}
+	return bbCells;
 }
 
 void printVecD(double* vec, int n) {
@@ -39,19 +53,22 @@ void printMatI(int** mat, int n, int m) {
 		printf("%8d ", mat[i][j]);
 		}
 	printf("\n");
-	}	
+	}
+	printf("\n");
 }
 void printMatD(double** mat, int n, int m) {
+	printf("");
 	for (int i = 0; i < n; i++){
 		for (int j = 0; j < m; j++){
-		printf("%8f ", mat[i][j]);
+		printf("%5.0f ", mat[i][j]);
 		}
 	printf("\n");
-	}	
+	}
+printf("\n");	
 }
 
 double** initUx(int nx,int ny,double uIn) {
-	double H = (double) ny-1;
+	double H = (double) ny-2;
 	int i,j;
 	double y;
 	
@@ -63,7 +80,7 @@ double** initUx(int nx,int ny,double uIn) {
 	
 	for (i = 0; i < nx; i++){
 		for (j = 0; j < ny; j++) {
-			y = (double) j;
+			y = j-0.5;//Change??
 			ux[i][j] = (4*uIn/(H*H))*(y*H-y*y);
 		}
 	}
@@ -79,7 +96,7 @@ double** initUy(int nx,int ny) {
 	return uy;
 }
 
-double*** initFin(int nf,int nx, int ny, int* ex, int* ey,
+double*** initFin(int nf,int nx, int ny, double* ex, double* ey,
 				double** ux, double** uy, double* w, double** rho) {
 	int i,j,k;
 	double u;
@@ -96,7 +113,7 @@ double*** initFin(int nf,int nx, int ny, int* ex, int* ey,
 	for (k = 0; k < nf; k++){
 		for (i = 0; i < nx; i++){
 			for (j = 0; j < ny; j++){
-			u = 3*ex[k]*ux[i][j] + ey[k]*uy[i][j];
+			u = 3*(ex[k]*ux[i][j] + ey[k]*uy[i][j]);
 			fIn[k][i][j] = rho[i][j]*w[k]*(1+u+0.5*u*u-1.5*(ux[i][j]*ux[i][j]+uy[i][j]*uy[i][j]));
 			}
 		}
@@ -143,7 +160,7 @@ void updateRho(double** rho, double*** fIn, int nx, int ny, int nf) {
 }
 
 void updateU(double** ux, double** uy, double*** fIn,
-			 double** rho, int* ex, int* ey, int nx, int ny, int nf){
+			 double** rho, double* ex, double* ey, int nx, int ny, int nf){
 	for (int i = 0; i < nx; i++) {//ARE THE LIMITS CORRECT? (1:ny-1)??
 		for (int j = 0; j < ny; j++){
 			ux[i][j] = 0;
@@ -158,9 +175,9 @@ void updateU(double** ux, double** uy, double*** fIn,
 
 void inlet(double** ux, double** uy, double** rho, double*** fIn, double uIn, int nx, int ny) {
 	double y, sum1, sum2;
-	double H = (double) ny-1;
+	double H = (double) ny-2;
 	for (int j = 1; j < (ny-1);j++) {
-		y = (double) j;
+		y = j-0.5;//Change??
 		//Poiseuille and Zou/He BC
 		ux[0][j] = (4*uIn/(H*H))*(y*H-y*y);
 		uy[0][j] = 0;
@@ -183,7 +200,7 @@ void outlet(double** ux, double** uy, double** rho, double*** fIn, int nx, int n
 		rho[nx-1][j] = 1;
 		sum1 = fIn[0][nx-1][j] + fIn[2][nx-1][j] + fIn[4][nx-1][j];
 		sum2 = fIn[1][nx-1][j] + fIn[5][nx-1][j] + fIn[8][nx-1][j];
-		ux[nx-1][j] = -1 + sum1 + 2*sum2;//UNCERTAIN!
+		ux[nx-1][j] = -1 + 1./(rho[nx-1][j])*(sum1 + 2*sum2);//UNCERTAIN!
 		uy[nx-1][j] = 0;
 		fIn[3][nx-1][j] = fIn[1][nx-1][j] - (2.0/3)*rho[nx-1][j]*ux[nx-1][j];
 		fIn[7][nx-1][j] = fIn[5][nx-1][j] + 0.5*(fIn[2][nx-1][j]-fIn[4][nx-1][j])
@@ -195,16 +212,17 @@ void outlet(double** ux, double** uy, double** rho, double*** fIn, int nx, int n
 	}
 }
 
-void collide(double** ux, double** uy, double***fIn, double*** fOut, double** rho, int* ex,
-			 int* ey, int nx,int ny, int nf, double tau, double* w){
-	double u;
+void collide(double** ux, double** uy, double***fIn, double*** fOut, double** rho, double* ex,
+			 double* ey, int nx,int ny, int nf, double tau, double* w){
+	double u, fEq;
 	int i,j,k;
 	for (k = 0; k < nf; k++){
 		for (i = 0; i < nx; i++){
 			for (j = 0; j < ny; j++){
-			u = 3*ex[k]*ux[i][j] + ey[k]*uy[i][j];
-			fOut[k][i][j] = fIn[k][i][j]-tau*rho[i][j]*w[k]*(1+u+0.5*u*u
+			u = 3*(ex[k]*ux[i][j] + ey[k]*uy[i][j]);
+			fEq = rho[i][j]*w[k]*(1+u+0.5*u*u
 			-1.5*(ux[i][j]*ux[i][j]+uy[i][j]*uy[i][j]));
+			fOut[k][i][j] = fIn[k][i][j]-tau*(fIn[k][i][j]-fEq);
 			}
 		}
 	}			 
@@ -212,6 +230,8 @@ void collide(double** ux, double** uy, double***fIn, double*** fOut, double** rh
 
 void bounce(double*** fIn, double*** fOut,int nx,int ny,int nf, int** bbCells, int nBBcells, int* opposite){
 	int i,j,k,l;
+	
+	//Obstacle
 	for (l = 0; l < nBBcells; l++) {
 		for (k = 0; k < nf; k++){
 				i = bbCells[0][l];
@@ -219,4 +239,114 @@ void bounce(double*** fIn, double*** fOut,int nx,int ny,int nf, int** bbCells, i
 				fOut[k][i][j] = fIn[opposite[k]][i][j];
 		}
 	}
-}		 
+	
+	//Top/Bottow wall
+	for (i = 0; i < nx; i++) {
+		for (k = 0; k < nf; k++){
+			fOut[k][i][0] = fIn[opposite[k]][i][0];
+			fOut[k][i][ny] = fIn[opposite[k]][i][ny];
+		}
+	}
+}
+
+void stream(double*** fIn, double*** fOut,int* ex,int* ey, int nx, int ny, int nf) {
+	//What to do with boundary nodes? Circular shift?
+	int i,j,k;
+
+	//Interior
+	for (i = 1; i < nx-1; i++) {
+		for (j = 1; j < ny-1; j++) {
+			for (k = 0; k < nf; k++) {
+				fIn[k][i][j] = fOut[k][i-ex[k]][j-ey[k]];
+			}
+		}
+	}	
+	//Outlet
+	for (j = 1; j < ny-1; j++) {
+		for (k = 0; k < nf; k++) {
+			if (ex[k]==-1) fIn[k][nx-1][j] = fOut[k][0][j-ey[k]];
+			else fIn[k][nx-1][j] = fOut[k][nx-1-ex[k]][j-ey[k]];
+		}
+	}
+	//Inlet
+	for (j = 1; j < ny-1; j++) {
+		for (k = 0; k < nf; k++) {
+			if (ex[k]==1) fIn[k][0][j] = fOut[k][nx-1][j-ey[k]];
+			else fIn[k][0][j] = fOut[k][-ex[k]][j-ey[k]];
+		}
+	}
+	//Top wall
+	for (i = 1; i < nx-1; i++) {
+		for (k = 0; k < nf; k++) {
+			if (ey[k]==-1) fIn[k][i][ny-1] = fOut[k][i-ex[k]][0];
+			else fIn[k][i][ny-1] = fOut[k][i-ex[k]][ny-1-ey[k]];
+		}
+	}
+	//Bottom wall
+	for (i = 1; i < nx-1; i++) {
+		for (k = 0; k < nf; k++) {
+			if (ey[k]==1) fIn[k][i][0] = fOut[k][i-ex[k]][ny-1];
+			else fIn[k][i][0] = fOut[k][i-ex[k]][-ey[k]];
+		}
+	}
+	//Top right corner
+	for (k = 0; k < nf; k++) {
+			if (ex[k]==-1){
+				if (ey[k]==-1) fIn[k][nx-1][ny-1] = fOut[k][0][0];
+				else fIn[k][nx-1][ny-1] = fOut[k][0][ny-1-ey[k]];
+			}
+			else if (ey[k]==-1) fIn[k][nx-1][ny-1] = fOut[k][nx-1-ex[k]][0];
+			else fIn[k][nx-1][ny-1] = fOut[k][nx-1-ex[k]][ny-1-ey[k]];
+		}		
+	//Bottom right corner
+	for (k = 0; k < nf; k++) {
+			if (ex[k]==-1){
+				if (ey[k]==1) fIn[k][nx-1][0] = fOut[k][0][ny-1];
+				else fIn[k][nx-1][0] = fOut[k][0][-ey[k]];
+				}
+			else if (ey[k]==1) fIn[k][nx-1][0] = fOut[k][nx-1-ex[k]][ny-1];
+			else fIn[k][nx-1][0] = fOut[k][nx-1-ex[k]][-ey[k]];
+		}	
+	//Top left corner
+	for (k = 0; k < nf; k++) {
+			if (ex[k]==1){
+				if (ey[k]==-1) fIn[k][0][ny-1] = fOut[k][nx-1][0];
+				else fIn[k][0][ny-1] = fOut[k][nx-1][ny-1-ey[k]];
+			} 
+			else if (ey[k]==-1) fIn[k][0][ny-1] = fOut[k][-ex[k]][0];
+			else fIn[k][0][ny-1] = fOut[k][-ex[k]][ny-1-ey[k]];
+		}		
+	//Bottom left corner
+	for (k = 0; k < nf; k++) {
+			if (ex[k]==1){
+				if (ey[k]==1) fIn[k][0][0] = fOut[k][nx-1][ny-1];
+				else fIn[k][0][0] = fOut[k][nx-1][-ey[k]];
+				}
+			else if (ey[k]==1) fIn[k][0][0] = fOut[k][-ex[k]][ny-1];
+			else fIn[k][0][0] = fOut[k][-ex[k]][-ey[k]];
+		}
+}
+
+void csvWriteD(double** mat, int n, int m, char* path) {
+	FILE* fp = fopen(path,"w");
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < m; j++){
+		fprintf(fp,"%f", mat[i][j]);
+		if (j != m-1) fprintf(fp,",");
+		}
+	if (i != (n-1)) fprintf(fp,"\n");
+	}
+	fclose(fp);
+}
+
+void csvWriteI(int** mat, int n, int m, char* path) {
+	FILE* fp = fopen(path,"w");
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < m; j++){
+		fprintf(fp,"%d", mat[i][j]);
+		if (j != m-1) fprintf(fp,",");
+		}
+	if (i != (n-1)) fprintf(fp,"\n");
+	}
+	fclose(fp);
+}
