9b9efbea4db0cf8068b002440ca8fdb6310db44e
tobias <t_torben@hotmail.com>
2017-03-27 21:42:55 +0200

    created all input files for report

--- src/runAllSims.sh
+++ src/runAllSims.sh
@@ -1,3 +1,12 @@
  #!/bin/bash
-./lbm ../input/cavity_129_re100.in
-./lbm ../input/cavity_129_re1000.in
+mkdir cavity_re100 cylinder_re10 foil_neutral poiseuille cavity_re1000 cylinder_re100 foil_pitched couette cylinder_re50 foil_stall
+./lbm ../input/couette_31.in
+./lbm ../input/poiseuille_31.in
+./lbm ../input/foil_neutral.in
+./lbm ../input/foil_pitched.in
+./lbm ../input/foil_stall.in
+./lbm ../input/cylinder_401_re10.in
+./lbm ../input/cylinder_401_re50.in
+./lbm ../input/cylinder_401_re100.in
+./lbm ../input/cavity_501_re100.in
+./lbm ../input/cavity_501_re1000.in
