4101a7adbd3597bd50a333a78e02179c50c853c9
tobias <t_torben@hotmail.com>
2017-03-28 19:22:34 +0200

    Small modification of inputfiles

--- src/runAllSims.sh
+++ src/runAllSims.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
-mkdir cavity_re100 cylinder_re10 foil_neutral poiseuille cavity_re1000 cylinder_re100 foil_pitched couette cylinder_re50 foil_stall
+mkdir cavity_re100 cylinder_re1 foil_neutral poiseuille cavity_re1000 cylinder_re100 foil_pitched couette cylinder_re50 foil_stall
 ./lbm ../input/couette_31.in
 ./lbm ../input/poiseuille_31.in
 ./lbm ../input/foil_neutral.in
